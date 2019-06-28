dde17f182df0a83e09e3e26a3459eadd0c6c1dda
mingo <2324829677@qq.com>
2019-06-29 01:24:58 +0800

    新增加IP 白名单校验登录的代码

--- Plugin.php
+++ Plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * M-Code(明哥验证码) 1.0 for Typecho
+ * M-Code(明哥验证码) 2.0 for Typecho </br> Update time: <code style="padding: 2px 4px; font-size: 90%; color: #c7254e; background-color: #f9f2f4; border-radius: 4px;">2019-06-29</code>
  *
  * @package Mcode
  * @author Mingo
- * @version 1.0.0
+ * @version 2.0.0
  * @link https://0x50j.com/
  */
 
@@ -14,27 +14,30 @@ class Mcode_Plugin implements Typecho_Plugin_Interface
      public static function activate() {
          Typecho_Plugin::factory('mc')->render = array('Mcode_Plugin', 'render');
          Typecho_Plugin::factory('mc')->verify = array('Mcode_Plugin', 'verify');
+         return _t('插件启用成功,请在login.php代码登陆的位置插入嵌入点代码!!!');
      }
      
      /* 禁用插件方法 */
      public static function deactivate() {
-
+        return _t('插件禁用成功');
      }
       
      /* 插件配置方法 */
      public static function config(Typecho_Widget_Helper_Form $form) {
-          $mcode_int = new Typecho_Widget_Helper_Form_Element_Text('mcode_int', NULL, '4', _t('验证码字符数'), _t('为基准值，范围0~6，默认4'));
-          $mcode_string = new Typecho_Widget_Helper_Form_Element_Text('mcode_string', NULL, '0123456789', _t('验证码字符集'));
-//          $types = array(
-//             'number' => '仅数字',
-//             'sen' => '仅英文(小写)',
-//             'ben' => '仅英文(大写)',
-//             'all' => '数字+英文(大小写)',
-//          );
-//          $mcode_type = new Typecho_Widget_Helper_Form_Element_Select('mcode_type', $types, 'number', _t('验证类型'));
+          $mcode_int = new Typecho_Widget_Helper_Form_Element_Text('mcode_int', NULL, '4', _t('验证码字符数'), _t('验证码字符数，范围0~6，默认4'));
+          $mcode_string = new Typecho_Widget_Helper_Form_Element_Text('mcode_string', NULL, '0123456789', _t('验证码字符集'), _t('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'));
+          $access = array(
+            'enable' => '启用',
+            'disable' => '禁用',
+          );
+          $mcode_access = new Typecho_Widget_Helper_Form_Element_Select('mcode_access', $access, 'disable', _t('是否启用白名单'));
+          $mcode_whitelist = new Typecho_Widget_Helper_Form_Element_Text('mcode_whitelist', NULL, NULL, _t('IP白名单'), _t('例如:192.168.0.1,192.168.1.1  以英文逗号分隔'));
+          $mcode_jump_url = new Typecho_Widget_Helper_Form_Element_Text('mcode_jump_url', NULL, 'https://resource.0x50j.com/', _t('跳转域名'), _t(' IP白名单未通过验证需要跳转的Url地址,例如:https://resource.0x50j.com/'));
           $form->addInput($mcode_int);
           $form->addInput($mcode_string);
-//          $form->addInput($mcode_type);
+          $form->addInput($mcode_access);
+          $form->addInput($mcode_whitelist);
+          $form->addInput($mcode_jump_url);
      }
       
      /* 个人用户的配置方法 */
@@ -45,6 +48,24 @@ class Mcode_Plugin implements Typecho_Plugin_Interface
      /* 插件实现方法 */
      public static function render() {
           session_start();
+          if (Typecho_Widget::widget('Widget_Options')->plugin('Mcode')->mcode_access == 'enable') {
+            $client_ip=$_SERVER['REMOTE_ADDR'];
+            $white_ip = explode(',',Typecho_Widget::widget('Widget_Options')->plugin('Mcode')->mcode_whitelist);
+            for ($index=0; $index<count($white_ip); $index++) {
+                if ($client_ip == $white_ip[$index]) {
+                  $acc_result = 1;
+                  break;
+                } else {
+                  $acc_result = 0;
+                }
+            }
+
+            if ($acc_result == 0) {
+              $jump_url = Typecho_Widget::widget('Widget_Options')->plugin('Mcode')->mcode_jump_url;
+              header("location:$jump_url");
+              die;
+            }
+          }
           $_SESSION['captcha_string'] = Typecho_Widget::widget('Widget_Options')->plugin('Mcode')->mcode_string;
           $_SESSION['captcha_int'] = Typecho_Widget::widget('Widget_Options')->plugin('Mcode')->mcode_int;
           echo '<p style="text-align: left;">';
